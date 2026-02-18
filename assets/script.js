51a60d4db761310bcf6554a8c418dd9c1ba40368
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-02-18 15:38:18 +0100

    add humna code for js

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,48 @@
+//TRACCIA
+
+/*
+
+Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata  in cui mostrare una card per ciascun componente.
+
+
+Bonus
+
+Rendere l’esercizio responsive, mandando a capo le card
+Aggiungere un form di aggiunta membri che permetta di visualizzare il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team! 😀)
+
+Buon divertimento e confermate lettura come al solito
+
+ */
+
+
+
+//HUMAN CODE
+
+/*
+
+Cosa devo fare?
+
+-Identifico markup da generare
+-Seleziono nodi DOM che mi servira
+-Creo una variabile d'appoggio
+-Creo un ciclo nell'array di oggetti (solita prassi)
+-Creo una variabile con all'interno il nostro markup
+-Dico al ciclo di che ad ogni iterazione deve inserire il tutto dentro la variabile d'appoggio co le relative proprieta del ciclo
+-Unisco il contento della variabile markup a quello della variabile d'appoggio
+-InnerHTML sull nodo inseredo il contenuto della variabile d'appoggio
+------NEL CICLO------
+
+
+
+
+
+*/
+
+
+
+
+
+
 const teamMembers = [
   {
     name: "Marco Bianchi",
